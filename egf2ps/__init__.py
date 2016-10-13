a7656d581f56b7d11700194e94a33035e6573649
Zhixian Ma <zxma_sjtu@qq.com>
2016-10-13 15:24:36 +0800

    __init__.py: add initial script including arguments for setup.

---
+++ egf2ps/__init__.py
@@ -0,0 +1,22 @@
+"""
+Elliptical Gaussian filters to detect point sources in X-ray astronomical images.
+
+Copyright (c) 2016 Zhixian MA <zxma_sjtu@qq.com>
+MIT license
+"""
+
+# Arguments for setup()
+__pkgname__ = "egf2ps"
+__version__ = "0.0.1"
+__author__ = "Zhixian MA"
+__author_email__ = "zxma_sjtu@qq.com"
+__license__ = "MIT"
+__copyright = "Copyright (c) 2016 Zhixian MA"
+__url__ = "https://github.com/myinxd/egf2ps"
+__description__ = ("Elliptical Gaussian filters to detect point source in X-ray astronomical images")
+
+# Set logging handle
+import logging
+from logging import NullHandler
+
+logging.getLogger(__name__).addHandler(NullHandler())
