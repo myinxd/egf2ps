a09cc84c5a4efdff305161108077e20c958903f1
Zhixian Ma <zxma_sjtu@qq.com>
2016-10-18 22:55:35 +0800

    detector: fixed some mistakes.

--- egf2ps/detector/detector.py
+++ egf2ps/detector/detector.py
@@ -10,11 +10,9 @@ Methods
 """
 
 import os
-import sys
 import numpy as np
 
 from ..utils import utils
-from ..basiclass import PointSource
 from ..basiclass import EGFilter
 from ..basiclass import PeakDetector
 
@@ -27,7 +25,7 @@ class Detector:
         """Get configurations from configs"""
         # Input
         self.imgpath = self.Configs.getn_value('input/imgpath')
-        self.refpath = self.COnfigs.getn_value('input/refpath')
+        self.refpath = self.Configs.getn_value('input/refpath')
         # Filters
         scale_type = self.Configs.getn_value('filter/scale_type')
         if scale_type == 'custom':
@@ -61,15 +59,16 @@ class Detector:
             self.sigma_x = [4,8]
             self.sigma_y = [4,8]
         # Angle
-        angle_step = self.Configs.getn_value('filter/angle_step')
-        self.angle = np.array(0,2*np.pi,angle_step)
+        # angle_step = self.Configs.getn_value('filter/angle_step')
+        angle_step = 0.3
+        self.angle = np.arange(0,2*np.pi+angle_step,angle_step)
 
         # Peaks
         self.threshold = self.Configs.getn_value('peaks/threshold')
-        
+
         # Snr
         self.snrthrs = self.Configs.getn_value('snr/threshold')
-        
+
         # Output
         self.dirname = self.Configs.getn_value('output/dirname')
         self.save = self.Configs.getn_value('output/save')
@@ -77,20 +76,21 @@ class Detector:
     def get_potential(self):
         """Detect and get potential point sources """
         # Init
-        pd = PeakDetector(self.Configs)
+		# read image
+        imgmat = utils.img2mat(self.imgpath)
         for s in self.scale_x:
             for var_x in self.sigma_x:
                 for var_y in self.sigma_y:
                     for ang in self.angle:
                         egf = EGFilter(scale=(s,s),sigma=(var_x,var_y),angle=ang)
-                        pd.egfilter = egf
-                        if 'pot_list' not in locals().keys():       
+                        pd = PeakDetector(self.Configs,imgmat,egf)
+                        if 'pot_list' not in locals().keys():
                             pot_list = pd.get_pslist()
                         else:
                             pot_list = np.row_stack((pot_list,pd.get_pslist()))
-      
+
         return pot_list
-        
+
     def get_final(self):
         """Discard false detections and clustering ps"""
         # Init
@@ -101,18 +101,18 @@ class Detector:
         min_snr = snr_list.min()
         snr_list = (snr_list - min_snr)/(max_snr-min_snr)
         # Discard
-        snr_idx = np.where(snr_list >= self.snrthrs)
-        pslist_snr = pot_list[:,snr_idx]
+        snr_idx = np.where(snr_list >= self.snrthrs)[0]
+        pslist_snr = pot_list[snr_idx,:]
 
         # Clustering
-        pslist = utils.cluster(pslist_snr)
-        
+        pslist = utils.cluster(pslist_snr,8,5)
+
         if self.save:
             filepath = os.path.join(self.dirname,'pslist.reg')
             utils.mat2reg(pslist,filepath)
-        
+
         return pslist
-    
+
     def get_performance(self,pslist):
         """Compare detected pslist with the reference"""
         reflist = utils.reg2mat(self.refpath)
